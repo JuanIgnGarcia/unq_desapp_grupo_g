fea8ee215a76840e2eab83a1459cec05090ee68f
JuanIgnGarcia <juan.ignacio.garcia.02@gmail.com>
2024-10-02 22:36:07 -0300

    Exceptions

---
+++ src/main/kotlin/com/example/demo/exceptions/UserNotFoundException.kt
@@ -0,0 +1,3 @@
+package com.example.demo.exceptions
+
+class UserNotFoundException(message: String) : RuntimeException(message)
---
+++ src/main/kotlin/com/example/demo/exceptions/UserOfferCreationException.kt
@@ -0,0 +1,4 @@
+package com.example.demo.exceptions
+
+class UserOfferCreationException(message: String) : RuntimeException(message)
+
