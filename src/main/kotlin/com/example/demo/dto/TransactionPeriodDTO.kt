88d8ba0e78af2df79a08e8e8363facf86ddd2260
JuanIgnGarcia <juan.ignacio.garcia.02@gmail.com>
2024-10-08 19:42:58 -0300

    SonarCloud exclude DTOs

--- src/main/kotlin/com/example/demo/dto/TransactionPeriodDTO.kt
+++ src/main/kotlin/com/example/demo/dto/TransactionPeriodDTO.kt
@@ -1,7 +1,9 @@
 package com.example.demo.dto
 
 import java.util.Date
+import org.springframework.aot.generate.Generated
 
+@Generated
 class TransactionPeriodDTO (val date : Date,
                             val totalAmount: Double,
                             val totalAmountArgs :Double,
--- src/main/kotlin/com/example/demo/dto/UserDTO.kt
+++ src/main/kotlin/com/example/demo/dto/UserDTO.kt
@@ -1,5 +1,8 @@
 package com.example.demo.dto
 
+import org.springframework.aot.generate.Generated
+
+@Generated
 class UserDTO(
     val id: String,
     val nameUser: String,
